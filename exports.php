7936400431f03f227faa6d4d2bdb7c1eb9a308b1
Pedro Pereira <pedro000pereira@gmail.com>
2019-10-09 20:51:05 +0100

    Update exports.php

--- exports.php
+++ exports.php
@@ -102,7 +102,7 @@ require "db_files/DB_queries.php";
       
               </td>
                          <!-- In this case we have this rows. You can change to your convenience -->
-                        <td><?php echo $row['<EXPORT>']?></td>
+                        <td><?php echo $row['EXPORT']?></td>
   
                       </tr>
         <?php 
