175b10f6b1efb913a039f2e3d2e6dd16977307c9
Pedro Pereira <pedro000pereira@gmail.com>
2019-10-09 20:39:21 +0100

    Update DB_queries.php

--- db_files/DB_queries.php
+++ db_files/DB_queries.php
@@ -1,5 +1,6 @@
 <?php 
 
+# In this case we have this tables. You can change to your convenience
 
 /*Query to select all the records*/
 $sql2access = "Select * from <access_table>";
@@ -8,4 +9,4 @@ $sql2exports = "Select * from <exports_table>";
 
 $sql2clients = "Select * from <clients_table>";
 
-$sql2Showlogs = "Select * from audit";
+$sql2Showlogs = "Select * <audit_table>";
