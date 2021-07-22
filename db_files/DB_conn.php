a210d83f4c699d439fc39f19786b54d792a44d91
Pedro Pereira <pedro000pereira@gmail.com>
2021-07-22 17:50:52 +0100

    Update DB_conn.php

--- db_files/DB_conn.php
+++ db_files/DB_conn.php
@@ -1,7 +1,7 @@
 <?php
 
 	/*DB Connection -> user -- pass -- DB */
-    $conn = mysqli_connect('localhost', 'root', 'abc', 'nfs');
+    $conn = mysqli_connect('<host>', '<user>', '<pass>', '<db>');
         if (mysqli_connect_errno())
             {
                 echo "Failed to connect to MySQL: " . mysqli_connect_error();
